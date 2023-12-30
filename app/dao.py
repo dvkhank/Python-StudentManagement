7d3ee6f242209e1416c7f95a9d3caac1bce69080
Duong Van Khanh <dvkhanh.work@gmail.com>
2023-12-30 18:06:42 +0700

    update database and login

--- app/dao.py
+++ app/dao.py
@@ -1,14 +1,36 @@
-from app.models import User
-from app import app
+from app.models import Teacher,Student, SetOfPermission, Permission_SetOfPermission, Permission
+from app import app, db
 import hashlib
 
 
 def get_user_by_id(user_id):
-    return User.query.get(user_id)
+    return Teacher.query.get(user_id)
 
-def auth_user(username, password):
-    password = str(hashlib.md5(password.encode('utf-8')).hexdigest())
 
-    return User.query.filter(User.username.__eq__(username),
-                             User.password.__eq__(password)).first()
+def load_permission(permission_id):
+    permission = db.session.query(Permission, SetOfPermission, Permission_SetOfPermission).filter(
+        Permission.id == Permission_SetOfPermission.permission_id,
+        SetOfPermission.id == Permission_SetOfPermission.set_of_permission_id, SetOfPermission.id == permission_id
+    ).all()
+    return permission
+
+
+def load_setofpermission():
+    return SetOfPermission.query.all()
+
+
+def auth_user(username, password, permission):
+    # password = str(hashlib.md5(password.encode('utf-8')).hexdigest())
+    with app.app_context():
+        if permission == '1':
+            return Teacher.query.filter(Teacher.username.__eq__(username),
+                                        Teacher.password.__eq__(password)).first()
+        if permission == '2':
+            return Student.query.filter(Student.username.__eq__(username),
+                                        Student.password.__eq__(password)).first()
+        if permission == '3':
+            return Teacher.query.filter(Teacher.username.__eq__(username),
+                                        Teacher.password.__eq__(password)).first()
+
+
 
--- app/index.py
+++ app/index.py
@@ -1,28 +1,39 @@
 import math
-from flask import render_template, request, redirect, jsonify, url_for
+from flask import render_template, request, redirect, jsonify, url_for, session
 import dao
 from app import app, login
 from flask_login import login_user, logout_user, login_required, current_user
 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if not current_user.is_authenticated:
+        return redirect(url_for('user_login'))
+    permission_id = session['permission_id']
+    permission = dao.load_permission(permission_id=permission_id)
+    return render_template("index.html", permission=permission)
+
 
 @app.route("/user_login", methods=['post', 'get'])
 def user_login():
     if request.method.__eq__('POST'):
         username = request.form.get('username')
         password = request.form.get('password')
+        permission = request.form.get('permission')
+        user = dao.auth_user(username=username, password=password, permission=permission)
 
-        user = dao.auth_user(username=username, password=password)
         if user:
             login_user(user)
+            session['permission_id'] = user.permission
             return redirect(url_for("index"))
-    return render_template("user_login.html")
+    set_of_permission = dao.load_setofpermission()
+    return render_template("user_login.html", setofpermission=set_of_permission)
+
 
 @app.route("/user_register", methods=['post', 'get'])
 def user_register():
     return render_template("user_register.html")
+
 
 @app.route("/user_logout")
 def user_logout():
@@ -40,6 +51,13 @@ def admin_login():
     return redirect("/admin")
 
 
+@app.route("/profile")
+def profile_user():
+    if current_user.is_authenticated:
+        user = dao.get_user_by_id(current_user.id)
+    return render_template('profile.html', user=user)
+
+
 @login.user_loader
 def load_user(user_id):
     return dao.get_user_by_id(user_id)
@@ -47,4 +65,5 @@ def load_user(user_id):
 
 if __name__ == '__main__':
     from app import admin
+
     app.run(debug=True)
