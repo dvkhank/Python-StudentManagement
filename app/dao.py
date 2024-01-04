ceef4264472173890f4c0314e3387d2126bc1709
Duong Van Khanh <dvkhanh.work@gmail.com>
2024-01-04 10:10:25 +0700

    update code

--- app/dao.py
+++ app/dao.py
@@ -1,16 +1,25 @@
-from app.models import Teacher,Student, SetOfPermission, Permission_SetOfPermission, Permission
+from app.models import Teacher, Student, SetOfPermission, Permission_SetOfPermission, Permission, Admin, TypeOfPhone, \
+    Staff
 from app import app, db
 import hashlib
 
 
-def get_user_by_id(user_id):
-    return Teacher.query.get(user_id)
+def get_user_by_id(user_id, set_of_permission_id):
+    if set_of_permission_id == 1:
+        return Teacher.query.get(user_id)
+    if set_of_permission_id == 2:
+        return Student.query.get(user_id)
+    if set_of_permission_id == 3:
+        return Staff.query.get(user_id)
+    if set_of_permission_id == 4:
+        return Admin.query.get(user_id)
 
 
-def load_permission(permission_id):
+def load_permission(set_of_permission_id):
     permission = db.session.query(Permission, SetOfPermission, Permission_SetOfPermission).filter(
         Permission.id == Permission_SetOfPermission.permission_id,
-        SetOfPermission.id == Permission_SetOfPermission.set_of_permission_id, SetOfPermission.id == permission_id
+        SetOfPermission.id == Permission_SetOfPermission.set_of_permission_id,
+        SetOfPermission.id == set_of_permission_id
     ).all()
     return permission
 
@@ -19,18 +28,31 @@ def load_setofpermission():
     return SetOfPermission.query.all()
 
 
-def auth_user(username, password, permission):
-    # password = str(hashlib.md5(password.encode('utf-8')).hexdigest())
+def load_phonetype():
+    return TypeOfPhone.query.all()
+
+
+def add_user(last_name, first_name, date_of_birth, email, phone, phone_type, degree, username, password, permission):
     with app.app_context():
         if permission == '1':
+            teacher = Teacher(last_name=last_name, first_name=first_name, date_of_birth=date_of_birth, email=email,
+                              phone=phone, phone_type=phone_type,
+                              degree=degree, username=username, password=password, permission=permission)
+            db.session.add(teacher)
+            db.session.commit()
+
+
+def auth_user(username, password, set_of_permission):
+    with app.app_context():
+        if set_of_permission == '1':
             return Teacher.query.filter(Teacher.username.__eq__(username),
                                         Teacher.password.__eq__(password)).first()
-        if permission == '2':
+        if set_of_permission == '2':
             return Student.query.filter(Student.username.__eq__(username),
                                         Student.password.__eq__(password)).first()
-        if permission == '3':
-            return Teacher.query.filter(Teacher.username.__eq__(username),
-                                        Teacher.password.__eq__(password)).first()
-
-
-
+        if set_of_permission == '3':
+            return Staff.query.filter(Staff.username.__eq__(username),
+                                      Staff.password.__eq__(password)).first()
+        if set_of_permission == '4':
+            return Admin.query.filter(Admin.username.__eq__(username),
+                                      Admin.password.__eq__(password)).first()
--- app/index.py
+++ app/index.py
@@ -1,4 +1,6 @@
 import math
+from functools import wraps
+
 from flask import render_template, request, redirect, jsonify, url_for, session
 import dao
 from app import app, login
@@ -8,26 +10,31 @@ from flask_login import login_user, logout_user, login_required, current_user
 @app.route("/")
 def index():
     if not current_user.is_authenticated:
-        return redirect(url_for('user_login'))
-    permission_id = session['permission_id']
-    permission = dao.load_permission(permission_id=permission_id)
-    return render_template("index.html", permission=permission)
+        return redirect(url_for("user_login"))
+    else:
+        set_of_permission_id = session['set_of_permission_id']
+        list_of_permission = dao.load_permission(set_of_permission_id=set_of_permission_id)
+    return render_template("index.html", list_of_permission=list_of_permission)
 
 
 @app.route("/user_login", methods=['post', 'get'])
 def user_login():
+    error_message = None
     if request.method.__eq__('POST'):
         username = request.form.get('username')
         password = request.form.get('password')
-        permission = request.form.get('permission')
-        user = dao.auth_user(username=username, password=password, permission=permission)
-
+        set_of_permission = request.form.get('set_of_permission')
+        print(set_of_permission)
+        user = dao.auth_user(username=username, password=password, set_of_permission=set_of_permission)
         if user:
             login_user(user)
-            session['permission_id'] = user.permission
+            print(user.username)
+            session['set_of_permission_id'] = user.setofpermission
             return redirect(url_for("index"))
+        else:
+            error_message = 'There is no user like that'
     set_of_permission = dao.load_setofpermission()
-    return render_template("user_login.html", setofpermission=set_of_permission)
+    return render_template("user_login.html", set_of_permission=set_of_permission, error_message=error_message)
 
 
 @app.route("/user_register", methods=['post', 'get'])
@@ -38,6 +45,8 @@ def user_register():
 @app.route("/user_logout")
 def user_logout():
     logout_user()
+    if 'set_of_permission_id' in session:
+        del session['set_of_permission_id']
     return redirect(url_for("user_login"))
 
 
@@ -51,16 +60,57 @@ def admin_login():
     return redirect("/admin")
 
 
+@app.route("/manage_users", methods=['post', 'get'])
+def manage_users():
+    set_of_permission = dao.load_setofpermission()
+    type_of_phone = dao.load_phonetype()
+    if request.method.__eq__('POST'):
+
+        last_name = request.form.get("last_name")
+        first_name = request.form.get("first_name")
+        date_of_birth = request.form.get("date_of_birth")
+        email = request.form.get("email")
+        phone = request.form.get('phone')
+        phone_type = request.form.get('phone_type')
+        degree = request.form.get('degree')
+        username = request.form.get('username')
+        password = request.form.get("password")
+        set_of_permission_id = request.form.get('permission')
+        if (set_of_permission_id == 1):
+            degree = request.form.get('degree')
+            dao.add_user(last_name=last_name, first_name=first_name, date_of_birth=date_of_birth, email=email,
+                         phone=phone,
+                         phone_type=phone_type, degree=degree, username=username, password=password,
+                         set_of_permission_id=set_of_permission_id)
+        if (set_of_permission_id == 2):
+            dao.add_user(last_name=last_name, first_name=first_name, date_of_birth=date_of_birth, email=email,
+                         phone=phone,
+                         phone_type=phone_type, degree=degree, username=username, password=password,
+                         set_of_permission_id=set_of_permission_id)
+    return render_template('add_users.html', set_of_permission=set_of_permission, type_of_phone=type_of_phone)
+
+
 @app.route("/profile")
 def profile_user():
+    set_of_permission_id = session['set_of_permission_id']
     if current_user.is_authenticated:
-        user = dao.get_user_by_id(current_user.id)
+        user = dao.get_user_by_id(current_user.id, set_of_permission_id)
     return render_template('profile.html', user=user)
 
 
 @login.user_loader
 def load_user(user_id):
-    return dao.get_user_by_id(user_id)
+    set_of_permission_id = session['set_of_permission_id']
+    user = dao.get_user_by_id(user_id, set_of_permission_id)
+    return user
+
+
+# @app.context_processor
+# def common_response():
+#     permission_id = None
+#     if permission_id:
+#         permission = dao.load_permission(permission_id)
+#         return {"permission": permission}
 
 
 if __name__ == '__main__':
