dde7f58cb059ac49fd3820c53f8a8de9999345e2
Duong Van Khanh <dvkhanh.work@gmail.com>
2024-01-06 13:37:11 +0700

    fix 1 mieng

--- app/admin.py
+++ app/admin.py
@@ -1,40 +1,35 @@
-
 from app import app, db
 from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
 from flask_login import logout_user, current_user
 from flask import redirect
-from app.models import Student
-admin = Admin(app=app, name='Student Management', template_mode='bootstrap4')
-admin.add_view(ModelView(Student, db.session))
+from app.models import Student, Teacher, Subject, TypeOfPhone, Staff, SetOfPermission, Permission_SetOfPermission
+from wtforms import StringField, validators
 
-class AuthenticatedAdmin(ModelView):
-    def is_accessible(self):
-        return current_user.is_authenticated and current_user.setofpermission == 4
+admin = Admin(app=app, name="STUDENT MANAGEMENT", template_mode='bootstrap4')
 
 
+class BaseView(ModelView):
+    create_template = 'admin/models/create.html'
+    column_editable_list = ['phone', 'email']
+    column_list = ['last_name', 'first_name', 'date_of_birth', 'email', 'phone', 'phone_type', 'username', 'password',
+                   'active', 'setofpermission']
+    column_searchable_list = ['first_name', 'first_name']
+    column_filters = ['last_name', 'first_name', 'active', 'setofpermission']
 
-# class MyStudentsView(AuthenticatedUser):
-#     @expose("/")
-#     def index(self):
-#         return self.render('admin/student.html')
-#
-#
-# class MyClassesView(AuthenticatedUser):
-#     @expose("/")
-#     def index(self):
-#         return self.render('admin/class.html')
-#
-#
-# class MySubjectsView(AuthenticatedUser):
-#     @expose("/")
-#     def index(self):
-#         return self.render('admin/subject.html')
-#
-#
-# class LogoutView(AuthenticatedUser):
-#     @expose("/")
-#     def index(self):
-#         logout_user()
-#         return redirect("/admin")
 
+class TypeOfPhoneView(ModelView):
+    column_list = ['id', 'name', 'student']
+
+
+class SetOfPermissionView(ModelView):
+    column_list = ['name', 'student', 'teacher', 'permission_setofpermission']
+
+
+admin.add_view(BaseView(Student, db.session, name="Manage Students"))
+admin.add_view(ModelView(Teacher, db.session))
+admin.add_view(ModelView(Staff, db.session))
+admin.add_view(ModelView(Subject, db.session))
+admin.add_view(TypeOfPhoneView(TypeOfPhone, db.session))
+admin.add_view(SetOfPermissionView(SetOfPermission, db.session))
+admin.add_view(ModelView(Permission_SetOfPermission, db.session))
